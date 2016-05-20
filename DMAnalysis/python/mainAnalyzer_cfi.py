2d9dabcdcbae11b9295aee3e6ce529e8eae34e44
Andreas Albert <albert@physik.rwth-aachen.de>
2016-05-20 16:34:21 +0200

    MainAnalyzer + config: whitespace.

--- DMAnalysis/python/mainAnalyzer_cfi.py
+++ DMAnalysis/python/mainAnalyzer_cfi.py
@@ -46,29 +46,29 @@ process.mainAnalyzer = cms.EDAnalyzer('MainAnalyzer',
     muonsTag = cms.InputTag("slimmedMuons"),
 
     electronsTag = cms.InputTag("slimmedElectrons"),
-	## 25ns
-    	electronVetoIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-veto"),
-	electronLooseIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-loose"),
-	electronMediumIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-medium"),
-    	electronTightIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-tight"),
-	electronHEEPIdTag = cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV60"),
-	#
-	# ID decisions (common to all formats)
-	#
-	eleMediumIdMapTrig = cms.InputTag("egmGsfElectronIDs:mvaEleID-Spring15-25ns-Trig-V1-wp80"),
-	eleTightIdMapTrig  = cms.InputTag("egmGsfElectronIDs:mvaEleID-Spring15-25ns-Trig-V1-wp90"),
-	#
-	# ValueMaps with MVA results
-	#
-	mvaValuesMapTrig     = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring15Trig25nsV1Values"),
-	mvaCategoriesMapTrig = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring15Trig25nsV1Categories"),
+    ## 25ns
+    electronVetoIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-veto"),
+    electronLooseIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-loose"),
+    electronMediumIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-medium"),
+    electronTightIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-tight"),
+    electronHEEPIdTag = cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV60"),
+    #
+    # ID decisions (common to all formats)
+    #
+    eleMediumIdMapTrig = cms.InputTag("egmGsfElectronIDs:mvaEleID-Spring15-25ns-Trig-V1-wp80"),
+    eleTightIdMapTrig  = cms.InputTag("egmGsfElectronIDs:mvaEleID-Spring15-25ns-Trig-V1-wp90"),
+    #
+    # ValueMaps with MVA results
+    #
+    mvaValuesMapTrig     = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring15Trig25nsV1Values"),
+    mvaCategoriesMapTrig = cms.InputTag("electronMVAValueMapProducer:ElectronMVAEstimatorRun2Spring15Trig25nsV1Categories"),
 
-	## 50ns
-#    	electronVetoIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-veto"),
-#	electronLooseIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-loose"),
-#	electronMediumIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-medium"),
-#    	electronTightIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-tight"),
-#	electronHEEPIdTag = cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV51"),
+    ## 50ns
+#       electronVetoIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-veto"),
+#   electronLooseIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-loose"),
+#   electronMediumIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-medium"),
+#       electronTightIdTag = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-CSA14-50ns-V1-standalone-tight"),
+#   electronHEEPIdTag = cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV51"),
 
 
     tausTag = cms.InputTag("slimmedTaus"),
@@ -97,24 +97,24 @@ process.mainAnalyzer = cms.EDAnalyzer('MainAnalyzer',
     prescales = cms.InputTag("patTrigger"),
     objects = cms.InputTag("selectedPatTrigger"),
 
-    	DoubleMuTrigs = cms.vstring("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v",
-					"HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v"
-				   ),
+    DoubleMuTrigs = cms.vstring("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v",
+                "HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v"
+               ),
 
-    	DoubleEleTrigs = cms.vstring("HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
-					),
+    DoubleEleTrigs = cms.vstring("HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
+                ),
 
-    	SingleMuTrigs = cms.vstring("HLT_IsoMu20_v",
-					"HLT_IsoTkMu20_v"),
+    SingleMuTrigs = cms.vstring("HLT_IsoMu20_v",
+                "HLT_IsoTkMu20_v"),
 
-    	SingleEleTrigs = cms.vstring(#"HLT_Ele27_eta2p1_WPLoose_Gsf_v",
-					"HLT_Ele23_WPLoose_Gsf_v",
-					#"HLT_Ele22_eta2p1_WPLoose_Gsf_v"
-					),
+    SingleEleTrigs = cms.vstring(#"HLT_Ele27_eta2p1_WPLoose_Gsf_v",
+                "HLT_Ele23_WPLoose_Gsf_v",
+                #"HLT_Ele22_eta2p1_WPLoose_Gsf_v"
+                ),
 
-   	MuEGTrigs = cms.vstring("HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v",
-					"HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v"),
+    MuEGTrigs = cms.vstring("HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v",
+                    "HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v"),
 
-	#DoubleTauTrigs = cms.vstring("HLT_DoubleMediumIsoPFTau40_Trk1_eta2p1_Reg_v")
+    #DoubleTauTrigs = cms.vstring("HLT_DoubleMediumIsoPFTau40_Trk1_eta2p1_Reg_v")
 )
 
