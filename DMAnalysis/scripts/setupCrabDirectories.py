d3bd0d7ccb59d4db0b3f7d2605ed37aeaf12cc25
amohamed <amohamed@ui03.recas.ba.infn.it>
2016-07-19 20:23:36 +0200

    correct the Data Json file Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt

--- DMAnalysis/scripts/setupCrabDirectories.py
+++ DMAnalysis/scripts/setupCrabDirectories.py
@@ -57,7 +57,7 @@ def parseCommandline():
         options.splitting_mc    = 'FileBased'
         options.json_mc         = ''
         options.splitting_data  = 'FileBased'
-        options.json_data       = os.path.expandvars( '$CMSSW_BASE/src/llvvAnalysis/DMAnalysis/data/Cert_271036-274421_13TeV_PromptReco_Collisions16_JSON.txt')
+        options.json_data       = os.path.expandvars( '$CMSSW_BASE/src/llvvAnalysis/DMAnalysis/data/Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt')
         options.outpath         = os.path.join( options.outpath, 'llvv_80', options.skimtag )
         options.workarea        = os.path.join( options.workarea, 'llvv_80', options.skimtag )
     else:
